2876b5f3558b0c3f83a979d2e41deb1c8b468f46
yozora <128562373+louwisalfred@users.noreply.github.com>
2024-01-22 17:52:57 +0800

    Add files via upload

---
+++ day20.py
@@ -0,0 +1,28 @@
+# for i in range(100,105):
+#     print(i)
+
+# for i in range(1,7):
+#     print('Day:',i)
+
+# print('Thirteen Times Table')
+# for i in range(1,13):
+#     print(i, 'x 13 =', i*13)
+
+
+# for i in range(10, -1, -1):
+#     print(i)
+
+# Forward
+# for i in range(0, 10):
+#     print(i+1)
+    
+# Backward
+# for i in range(10, 0,-1):
+#     print(i)
+    
+x = int(input('Start at: '))
+y = int(input('End at: '))
+z = int(input('Enter val: '))
+
+for i in range(x, y ,z):
+    print(i+z)
